ede7b7f57cead42222fc5de30459516c4c953a3a
MuthuKarthikBala <46017272+MuthuKarthikBala@users.noreply.github.com>
2018-12-22 20:30:10 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,31 @@
+# Program to concatenating strings
+print("Program to concatenate 2 strings")
+x = str(input("Enter string1: "))
+y = str(input("Enter string2: "))
+print("Concatenated string is ", x+y)
+
+# Program to add 2 user numbers
+print("Program to add 2 numbers")
+x = int(input("Enter number1: "))
+y = int(input("Enter number2: "))
+print("Sum of two numbers is :", x+y)
+
+# Program to find maximum of 2 numbers
+print("Program to find maximum of 2 numbers")
+x = int(input("Enter number1: "))
+y = int(input("Enter number2: "))
+print("Maximum of ", x, "and", y, "is", max(x,y))
+
+# Program to find power of numbers using from math import *
+from math import *
+print("Program to find power of 2 numbers using pow")
+x = int(input("Enter number1: "))
+y = int(input("Enter number2: "))
+print("Power of x ^ y is : ", pow(x,y))
+
+# Program to find power of numbers using math import
+import math
+print("Program to find power of 2 numbers using math module")
+x = int(input("Enter number1: "))
+y = int(input("Enter number2: "))
+print("Power of x ^ y is : ", math.pow(x,y))
